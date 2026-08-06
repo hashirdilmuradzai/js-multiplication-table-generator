fd60aef5ff0c540e93d1d9dafd21e5ad1809d6fb
Hashir Dilmurad Zai <dzhashirdilmuradzai12@gmail.com>
2026-08-07 00:37:00 +0500

    Completed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function printTable(event) {
-    event.preventDefault();
-    const tableNumber = document.querySelector("#tableNumber");
+  event.preventDefault();
 
-    for (let i = 1; i <= 12; i++) {
-        const tableOf = Number(tableNumber.value)
+  const tableNumber = document.querySelector("#tableNumber");
+  const tableOutput = document.querySelector("#tableOutput");
+  const tableLimit = document.querySelector("#tableLimit");
+  const tableEnd = Number(tableLimit.value);
+  const tableOf = Number(tableNumber.value);
 
-        console.log(tableOf + " x " + i + " = " + tableOf * i);
-    }
+  tableOutput.innerHTML = "";
+
+  for (let i = 1; i <= tableEnd; i++) {
+    tableOutput.innerHTML += tableOf + " x " + i + " = " + tableOf * i + "<br>";
+  }
 }
